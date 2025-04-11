a17a489ea61acbfb84bffd9b103ed66f881f024d
Park Seokmin <109731052+bingbingbingsu@users.noreply.github.com>
2025-04-12 00:41:07 +0900

    Feat: add state.gd
    - 분기를 저장

---
+++ demo/global/state.gd
@@ -0,0 +1,14 @@
+extends Node
+
+var current_node = "start"
+var story= []
+var choices = {}
+
+func save_node(node:String):
+	story.append(node)
+
+func save_choice(key: String, value):
+	choices[key] = value
+
+func get_choice(key: String):
+	return choices.get(key, null)
