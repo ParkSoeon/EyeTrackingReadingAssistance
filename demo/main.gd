7b32e92f00b1aecc26a92e5f00145d08e2a25c29
Park Seokmin <109731052+bingbingbingsu@users.noreply.github.com>
2025-05-11 13:34:28 +0900

    Feat: parallel background

--- demo/main.gd
+++ demo/main.gd
@@ -8,6 +8,8 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	var position = get_node("Player").position
+	print(position)
 	pass
 
 
