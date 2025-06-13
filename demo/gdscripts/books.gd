127004158f55398fdf2dafbb79ddb04d3ef8c379
Park Seokmin <109731052+bingbingbingsu@users.noreply.github.com>
2025-06-13 20:49:44 +0900

    Feat: fade in, out

--- demo/gdscripts/books.gd
+++ demo/gdscripts/books.gd
@@ -7,5 +7,6 @@ func _ready() -> void:
 func _on_texture_button_pressed() -> void: # 임시
 	STATE.set_story("story")
 	await HttpClient.post_play(STATE.get_story())
-	get_tree().change_scene_to_file("res://scenes/story.tscn")
+	SceneTransitionLayer.change_scene_with_fade("res://scenes/story.tscn")
+	#get_tree().change_scene_to_file("res://scenes/story.tscn")
 	pass # Replace with function body.
