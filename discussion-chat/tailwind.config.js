39e8527ffaede27337200b1b66328ebd981e3edd
ParkSoeon <106726934+ParkSoeon@users.noreply.github.com>
2025-06-15 17:15:38 +0900

    Update tailwind.config.js

--- discussion-chat/tailwind.config.js
+++ discussion-chat/tailwind.config.js
@@ -1,14 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
-// tailwind.config.js
-module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
